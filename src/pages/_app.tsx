1b66f434f116014647e961818fa71833fedf01e0
SIDANWhatever <wongkahinhinson@gmail.com>
2024-08-26 12:14:37 +0800

    feat: update template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import "@meshsdk/react/styles.css";
 import type { AppProps } from "next/app";
 import { MeshProvider } from "@meshsdk/react";
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,12 @@ export default function Home() {
         <meta name="description" content="A Cardano dApp powered my Mesh" />
       </Head>
       <main
-        className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className} `}
-      >
-
+        className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className} `}>
         <h1 className="text-6xl font-thin mb-20">
-          <a href="https://meshjs.dev/" className="text-sky-600">Mesh</a> Next.js
+          <a href="https://meshjs.dev/" className="text-sky-600">
+            Mesh
+          </a>{" "}
+          Next.js
         </h1>
 
         <div className="mb-20">
@@ -24,7 +25,9 @@ export default function Home() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 content-center justify-around ">
-          <a href="https://meshjs.dev/apis" className="bg-gray-800 rounded-xl border border-white hover:scale-105 transition max-w-96 p-5 m-5">
+          <a
+            href="https://meshjs.dev/apis"
+            className="bg-gray-800 rounded-xl border border-white hover:scale-105 transition max-w-96 p-5 m-5">
             <h2 className="text-2xl font-bold mb-2">Documentation</h2>
             <p className="text-gray-400">
               Our documentation provide live demos and code samples; great
@@ -32,7 +35,9 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://meshjs.dev/guides" className="bg-gray-800 rounded-xl border border-white hover:scale-105 transition max-w-96  p-5 m-5">
+          <a
+            href="https://meshjs.dev/guides"
+            className="bg-gray-800 rounded-xl border border-white hover:scale-105 transition max-w-96  p-5 m-5">
             <h2 className="text-2xl font-bold mb-2">Guides</h2>
             <p className="text-gray-400">
               Whether you are launching a new NFT project or ecommerce store,
@@ -40,7 +45,9 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://meshjs.dev/react" className="bg-gray-800 rounded-xl border border-white hover:scale-105 transition max-w-96 p-5 m-5 md:mx-auto lg:mx-5 md:col-span-2 lg:col-span-1 ">
+          <a
+            href="https://meshjs.dev/react"
+            className="bg-gray-800 rounded-xl border border-white hover:scale-105 transition max-w-96 p-5 m-5 md:mx-auto lg:mx-5 md:col-span-2 lg:col-span-1 ">
             <h2 className="text-2xl font-bold mb-2">React components</h2>
             <p className="text-gray-400">
               Useful React UI components and hooks, seamlessly integrate them
@@ -50,7 +57,7 @@ export default function Home() {
         </div>
       </main>
       <footer className="p-8 border-t border-gray-300 flex justify-center">
-        <MeshBadge dark={true} />
+        <MeshBadge isDark={true} />
       </footer>
     </div>
   );
