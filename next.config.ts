fe9f8d84c796116f10ce850206a0fba87619a27f
Jingles <jinglescode@gmail.com>
2025-01-27 16:46:07 +0800

    1.9.0

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack: function (config, options) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
